7b5653d4ce3cb4117b1f7754f51a400a73773522
wangyefei <1626607072@qq.com>
2022-10-21 21:16:59 +0800

    dp-medium-001

--- leetcode/dynamic-programming/medium/01/2002_max_product.py
+++ leetcode/dynamic-programming/medium/01/2002_max_product.py
@@ -7,12 +7,26 @@
 class Solution:
     def maxProduct(self, s: str) -> int:
         n = len(s)
-        for i in range(1<<n):
-            s1 = ""
-            s2 = ""
-            for k in range(n):
-                if i&(1>>k):
-                    s1 += s[i]
-                else:
-                    s2 += s[i]
+        palins = []  # 哈希表，保存所有的回文数字
+        for num in range(1, 2**n):
+            arr = []
+            for j in range(n):
+                if num & (1 << j):
+                    arr.append(s[j])
+            # 检查数字映射的序列是否为回文序列，如果是，将其加入数组
+            isPalin = True
+            for j in range(len(arr) // 2):
+                if arr[j] != arr[-j - 1]:
+                    isPalin = False
+                    break
+            if isPalin:
+                palins.append(num)
+        # 下面开始找到长度乘积最大的不相交回文串
+        ans = 0
+        for i in range(len(palins)):
+            for j in range(i + 1, len(palins)):
+                if palins[i] & palins[j] == 0:
+                    ans = max(ans, palins[i].bit_count() * palins[j].bit_count())
+        return ans
+
 
