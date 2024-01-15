4a04e95aa4bf64e75596978805418a9941239cec
yfwang <1626607072@qq.com>
2024-01-15 17:47:48 +0800

    state machine

--- codeforces/mock.py
+++ codeforces/mock.py
@@ -1,7 +1,11 @@
+import random
 import sys
 
 sys.stdout = open('./input.txt', 'w')
-print(1)
-print(1000, 1000000)
-print(*[1000 for _ in range(1000)])
-print(*[1000 for _ in range(1000)])
+tcn = 100
+print(tcn)
+for _tcn_ in range(tcn):
+    n, m = 5, 5
+    print(n, m)
+    nums = [random.randint(1, 2) for _ in range(n)]
+    print(*nums)
---
+++ codeforces/untag/1615C.py
@@ -0,0 +1,46 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = I()
+for _tcn_ in range(tcn):
+    n = I()
+    s1 = input()
+    s2 = input()
+    if s1 == s2:
+        print(0)
+        continue
+    if s1.count('1') == 0:
+        print(-1)
+        continue
+
+    x00 = x01 = x11 = x10 = 0
+    for i in range(n):
+        if s1[i] == '0' and s2[i] == '0':
+            x00 += 1
+        if s1[i] == '0' and s2[i] == '1':
+            x01 += 1
+        if s1[i] == '1' and s2[i] == '1':
+            x11 += 1
+        if s1[i] == '1' and s2[i] == '0':
+            x10 += 1
+
+    ans1 = ans2 = 1 << 30
+    if x11 == x00 + 1:
+        ans1 = x11 + x00
+    if x10 == x01:
+        ans2 = x10 + x01
+    ans = min(ans1, ans2)
+    if ans == 1 << 30:
+        print(-1)
+    else:
+        print(ans)
