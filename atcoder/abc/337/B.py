886eaf8696e0d2575fa44974acd8e9bf32466c53
wyf <1626607072@qq.com>
2024-01-21 10:28:52 +0800

    atcoder 337 G DFS欧拉序 树状数组 逆序对 树上差分

---
+++ atcoder/abc/337/B.py
@@ -0,0 +1,24 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/1/20 20:04
+# @Author: yefei.wang
+# @File: B.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+s = list(input())
+ss = sorted(s)
+if s == ss:
+    print('Yes')
+else:
+    print('No')
