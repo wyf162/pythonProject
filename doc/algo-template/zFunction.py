248f46a3cf054cb57a8e31246a79fa228174e721
yfwang <1626607072@qq.com>
2024-02-22 09:44:52 +0800

    432D z func prefix

---
+++ doc/algo-template/zFunction.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/2/22 9:40
+# @Author: yfwang
+# @File: zFunction.py
+
+def z_function(s):
+    n = len(s)
+    z = [0] * n
+    l, r = 0, 0
+    for i in range(1, n):
+        if i <= r and z[i - l] < r - i + 1:
+            z[i] = z[i - l]
+        else:
+            z[i] = max(0, r - i + 1)
+            while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+        if i + z[i] - 1 > r:
+            l = i
+            r = i + z[i] - 1
+    z[0] = n
+    return z
+
