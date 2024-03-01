b38e8310af9ed5ca0e2587ff0d5e60b94601f192
yfwang <1626607072@qq.com>
2024-03-01 11:03:58 +0800

    3D 反悔贪心

---
+++ codeforces/untag/3D.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/3/1 10:06
+# @Author: yfwang
+# @File: 3D.py
+# https://codeforces.com/problemset/problem/3/D
+
+import sys
+from heapq import heappush, heappop
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+s = list(input())
+n = len(s)
+m = s.count('?')
+costs = [LI() for _ in range(m)]
+j = 0
+cost = 0
+balance = 0
+
+h = []
+
+for i in range(n):
+    if s[i] == '(':
+        balance += 1
+    elif s[i] == ')':
+        balance -= 1
+    else:
+        a, b = costs[j]
+        j += 1
+        s[i] = ')'
+        cost += b
+        balance -= 1
+        heappush(h, (a-b, i))
+    if balance < 0:
+        if not h:
+            exit(print(-1))
+        diff, idx = heappop(h)
+        s[idx] = '('
+        cost += diff
+        balance += 2
+if balance == 0:
+    print(cost)
+    print(''.join(s))
+else:
+    print(-1)
+
+
+
