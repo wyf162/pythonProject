2c7f91b783b970b017255645493a398ca89d0cfd
wyf <1626607072@qq.com>
2024-04-04 10:55:17 +0800

    1468F 向量反向共线

---
+++ codeforces/math/1468F.py
@@ -0,0 +1,36 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/4/4 10:20
+# @Author: yefei.wang
+# @File: 1468F.py
+# https://codeforces.com/problemset/problem/1468/F
+
+import math
+import sys
+from collections import Counter
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = I()
+for _tcn_ in range(tcn):
+    n = I()
+    persons = [LI() for _ in range(n)]
+    ans = 0
+    cnt = Counter()
+    for x, y, u, v in persons:
+        x -= u
+        y -= v
+        d = math.gcd(x, y)
+        x //= d
+        y //= d
+        cnt[(x, y)] += 1
+        ans += cnt[(-x, -y)]
+    print(ans)
