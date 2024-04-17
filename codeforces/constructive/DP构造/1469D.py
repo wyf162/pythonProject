f5e17e92f2ff73c2b1a5edbb077c9185cc43188d
yfwang <1626607072@qq.com>
2024-04-17 13:59:13 +0800

    1469D 构造 子问题

---
+++ codeforces/constructive/DP构造/1469D.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/4/17 13:26
+# @Author: yfwang
+# @File: 1469D.py
+# https://codeforces.com/contest/1469/problem/D
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+
+def cd(x, y):
+    return (x + y - 1) // y
+
+
+tcn = I()
+for _tcn_ in range(tcn):
+    n = I()
+    out = []
+    large = n
+    for i in range(n - 1, 1, -1):
+        while cd(large, i) in [i, i+1]:
+            out.append([n, i])
+            large = cd(large, i)
+        out.append([i, n])
+    print(len(out))
+    for i in range(len(out)):
+        print(*out[i])
