ff69a394c2ae101774463a0ec4907017ae2e2b31
yfwang <1626607072@qq.com>
2024-04-18 16:22:17 +0800

    1010D 二叉树 路径

--- codeforces/tree/1010D.py
+++ codeforces/tree/1010D.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# @Time: 2024/4/18 13:25
+# @Time: 2024/4/18 15:16
 # @Author: yfwang
 # @File: 1010D.py
-# https://codeforces.com/problemset/problem/1010/D
 
 import sys
+from operator import xor, and_, not_, or_
 
 input = lambda: sys.stdin.readline().rstrip()
 sys.stdin = open('../input.txt', 'r')
@@ -17,40 +17,81 @@ YN = lambda x: print('YES' if x else 'NO')
 mod = 1000000007
 mod2 = 998244353
 
-import sys
+n = I()
+tree = [[] for _ in range(n + 1)]
+fa = [-1 for _ in range(n + 1)]
+ops = ['' for _ in range(n + 1)]
+nums = [-1 for _ in range(n + 1)]
 
-n = int(input())
-g = {
-    'A': lambda a, b: v[a] & v[b],
-    'O': lambda a, b: v[a] | v[b],
-    'X': lambda a, b: v[a] ^ v[b],
-    'N': lambda a: v[a] ^ 1,
-    'I': lambda a: a + 1
+# func map
+fm = {
+    'AND': and_,
+    'OR': or_,
+    'XOR': xor,
+    'NOT': not_
 }
 
+for i in range(1, n + 1):
+    s = input().split()
+    if s[0] in ['AND', 'XOR', 'OR']:
+        c1, c2 = int(s[1]), int(s[2])
+        tree[i] = [c1, c2]
+        fa[c1] = i
+        fa[c2] = i
+        ops[i] = s[0]
+    elif s[0] == 'NOT':
+        c1 = int(s[1])
+        tree[i] = [c1]
+        fa[c1] = i
+        ops[i] = s[0]
+    else:
+        c1 = int(s[1])
+        nums[i] = c1
 
-def f(q):
-    q = q.split()
-    return q.pop(0)[0], [int(t) - 1 for t in q]
+stk = [1]
+dfs = []
+while stk:
+    x = stk.pop()
+    dfs.append(x)
+    for y in tree[x]:
+        stk.append(y)
+# print(dfs[::-1])
+for x in dfs[::-1]:
+    if not ops[x]:
+        continue
+    nums[x] = fm[ops[x]](*[nums[y] for y in tree[x]])
+    nums[x] = int(nums[x])
+
+# print(nums)
+# print(nums[1])
+
+state = [0] * (n+1)
+stk = [1]
+while stk:
+    x = stk.pop()
+    state[x] = 1
+    if not ops[x]:
+        continue
+    if len(tree[x]) == 1:
+        stk.append(tree[x][0])
+    else:
+        c1, c2 = tree[x]
+        x1 = nums[c1] ^ 1
+        x2 = nums[c2] ^ 1
+        if fm[ops[x]](x1, nums[c2]) != nums[x]:
+            stk.append(c1)
+        if fm[ops[x]](nums[c1], x2) != nums[x]:
+            stk.append(c2)
+
+rets = []
+for i in range(1, n+1):
+    if not ops[i]:
+        if state[i]:
+            rets.append(nums[1] ^ 1)
+        else:
+            rets.append(nums[1])
+print(''.join(str(x) for x in rets))
 
 
-d = [f(q) for q in sys.stdin.readlines()]
-t = [0]
-for i in t:
-    f, a = d[i]
-    if f != 'I': t.extend(a)
-v = [0] * n
-for i in t[::-1]:
-    f, a = d[i]
-    v[i] = g[f](*a)
-s = [0] * n
-s[0] = 1
-for i in t:
-    if not s[i]: continue
-    f, a = d[i]
-    if f == 'I': continue
-    for k in a:
-        v[k] ^= 1
-        s[k] = g[f](*a) != v[i]
-        v[k] ^= 1
-print(''.join(str(q ^ v[0]) for q, (f, a) in zip(s, d) if f == 'I'))
+
+
