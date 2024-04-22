d75de91f076ab1434f4fb8d3e842ee1a7ef3e0a8
yfwang <1626607072@qq.com>
2024-04-22 15:51:16 +0800

    1200D 差分数组

--- codeforces/diff/1200D.py
+++ codeforces/diff/1200D.py
@@ -6,7 +6,7 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
-# sys.stdin = open('../input.txt', 'r')
+sys.stdin = open('../input.txt', 'r')
 # sys.stdout = open('../output.txt', 'w')
 I = lambda: int(input())
 MI = lambda: map(int, input().split())
@@ -16,27 +16,17 @@ LGMI = lambda: list(GMI())
 YN = lambda x: print('YES' if x else 'NO')
 mod = 1000000007
 
-tcn = 1
+tcn = I()
 for _tcn_ in range(tcn):
     n, k = MI()
     mtx = [[int(c == 'W') for c in input()] for _ in range(n)]
 
-    left = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
     right = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-    top = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
     down = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-
-    for i in range(n):
-        for j in range(n):
-            left[i][j + 1] = left[i][j] + mtx[i][j]
 
     for i in range(n):
         for j in range(n - 1, -1, -1):
             right[i][j] = right[i][j + 1] + mtx[i][j]
-
-    for i in range(n):
-        for j in range(n):
-            top[i + 1][j] = top[i][j] + mtx[i][j]
 
     for i in range(n - 1, -1, -1):
         for j in range(n):
@@ -55,26 +45,30 @@ for _tcn_ in range(tcn):
 
 
     for i in range(n):
+        acc = 0
         for j in range(n):
-            if left[i][j] == j and right[i][min(n, j + k)] >= n - j - k:
+            if acc == j and right[i][min(n, j + k)] >= n - j - k:
                 x1 = max(0, i - k + 1)
                 modify(x1, j, i, j)
+            acc += mtx[i][j]
 
-    for i in range(n):
-        for j in range(n):
-            if top[i][j] == i and down[min(n, i + k)][j] >= n - i - k:
+    for j in range(n):
+        acc = 0
+        for i in range(n):
+            if acc == i and down[min(n, i + k)][j] >= n - i - k:
                 y1 = max(0, j - k + 1)
                 modify(i, y1, i, j)
+            acc += mtx[i][j]
 
     for i in range(n):
-        if left[i][n] == n:
+        if right[i][0] == n:
             if i + 1 <= n - 1:
                 modify(i + 1, 0, n - 1, n - 1)
             if i - k >= 0:
                 modify(0, 0, i - k, n - 1)
 
     for j in range(n):
-        if top[n][j] == n:
+        if down[0][j] == n:
             if j + 1 <= n - 1:
                 modify(0, j + 1, n - 1, n - 1)
             if j - k >= 0:
--- codeforces/diff/demo.py
+++ codeforces/diff/demo.py
@@ -4,27 +4,27 @@
 # @File: demo.py
 
 n = 4
-diff = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-pre_sum = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
+f = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+g = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
 
 
 def modify(x1, y1, x2, y2):
     # 左闭右闭
-    diff[x1][y1] += 1
-    diff[x2 + 1][y2 + 1] += 1
-    diff[x1][y2 + 1] -= 1
-    diff[x2 + 1][y1] -= 1
+    f[x1][y1] += 1
+    f[x2 + 1][y2 + 1] += 1
+    f[x1][y2 + 1] -= 1
+    f[x2 + 1][y1] -= 1
 
 
 def acc():
     for i in range(n):
         for j in range(n):
-            pre_sum[i + 1][j + 1] = diff[i][j] + pre_sum[i + 1][j] + pre_sum[i][j + 1] - pre_sum[i][j]
+            g[i + 1][j + 1] = f[i][j] + g[i + 1][j] + g[i][j + 1] - g[i][j]
 
 
 def pp():
     for i in range(1, n + 1):
-        print(*pre_sum[i][1:-1])
+        print(*g[i][1:-1])
 
 
 if __name__ == '__main__':
