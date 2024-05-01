c77a3f5ba0df5a58f4453ed08603027b54fd6d9c
wyf <1626607072@qq.com>
2024-05-01 14:40:27 +0800

    631C implementation

--- codeforces/mock.py
+++ codeforces/mock.py
@@ -2,10 +2,13 @@ import random
 import sys
 
 sys.stdout = open('./input.txt', 'w')
-tcn = 100
+tcn = 1000
 print(tcn)
 for _tcn_ in range(tcn):
-    n = 10
-    print(n)
-    a = [random.randint(1, 10) for _ in range(n)]
+    n = 100
+    m = 100
+    print(n, m)
+    a = [random.randint(1, 30) for _ in range(n)]
     print(*a)
+    for i in range(m):
+        print(random.randint(1, 2), random.randint(1, 10))
