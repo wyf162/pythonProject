3838c33f591bbcad30ff016f6a77ccbbf3c35a17
wyf <1626607072@qq.com>
2024-05-01 20:33:05 +0800

    1972B game

--- codeforces/contest/2024/round-942-div2/B.py
+++ codeforces/contest/2024/round-942-div2/B.py
@@ -4,28 +4,6 @@
 # @File: B.py
 
 import sys
-from types import GeneratorType
-
-
-def bootstrap(f, stack=[]):
-    def wrappedfunc(*args, **kwargs):
-        if stack:
-            return f(*args, **kwargs)
-        else:
-            to = f(*args, **kwargs)
-            while True:
-                if type(to) is GeneratorType:
-                    stack.append(to)
-                    to = next(to)
-                else:
-                    stack.pop()
-                    if not stack:
-                        break
-                    to = stack[-1].send(to)
-            return to
-
-    return wrappedfunc
-
 
 input = lambda: sys.stdin.readline().rstrip()
 sys.stdin = open('../../../input.txt', 'r')
@@ -38,42 +16,11 @@ YN = lambda x: print('YES' if x else 'NO')
 mod = 1000000007
 mod2 = 998244353
 
-hst = dict()
-
-
-@bootstrap
-def dfs(ss):
-    if ss in ['U', 'UD', 'DU']:
-        yield True
-    elif ss in ['D', 'UU', 'DD']:
-        yield False
-    for i in range(len(ss)):
-        tt = ss[i:] + ss[:i]
-        if tt in hst:
-            yield hst[tt]
-
-    ans = False
-    for i, c in enumerate(ss):
-        if c == 'U':
-            t = ss[i + 1:] + ss[:i]
-            tt = ''
-            if t[0] == 'U':
-                tt += 'D'
-            else:
-                tt += 'U'
-            tt += t[1:-1]
-            if t[-1] == 'U':
-                tt += 'D'
-            else:
-                tt += 'U'
-            ans = ans or (not next(dfs(t)))
-    hst[ss] = ans
-    yield ans
-
-
 tcn = I()
 for _tcn_ in range(tcn):
     n = I()
-    nums = input()
-    ret = dfs(nums)
-    YN(ret)
+    c = input().count('U')
+    if c % 2:
+        YN(True)
+    else:
+        YN(False)
