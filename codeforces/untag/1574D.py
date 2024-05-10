aa95bb54ee460150292beca2a3971bffa3495cf3
yfwang <1626607072@qq.com>
2024-05-10 18:02:23 +0800

    1574D TLE

--- codeforces/untag/1574D.py
+++ codeforces/untag/1574D.py
@@ -4,6 +4,7 @@
 # @File: 1574D.py
 
 import sys
+from heapq import heappop, heappush
 
 input = lambda: sys.stdin.readline().rstrip()
 sys.stdin = open('../input.txt', 'r')
@@ -17,14 +18,54 @@ YN = lambda x: print('YES' if x else 'NO')
 mod = 1000000007
 mod2 = 998244353
 
-tcn = I()
+
+def f1(nums):
+    x = 0
+    for i in reversed(range(n)):
+        x *= N
+        x += nums[i]
+    return x
+
+
+def f2(x):
+    nums = []
+    for _ in range(n):
+        nums.append(x % N)
+        x //= N
+    return nums
+
+
+def get_sum(nums):
+    return sum(A[i][nums[i]] for i in range(n))
+
+
+tcn = 4
 for _tcn_ in range(tcn):
     n = I()
-    A = []
-    for _ in range(n):
-        _, *a = LI()
-        A.append(a)
+    A = [LI() for _ in range(n)]
+    N = max(len(a) for a in A) + 5
     m = I()
     B = [LI() for _ in range(m)]
+    st = set()
+    for b in B:
+        x = f1(b)
+        st.add(x)
 
+    cur = f1([a[0] for a in A])
+    h = []
+    heappush(h, (-get_sum(f2(cur)), cur))
 
+    while True:
+        val, cur = heappop(h)
+        if cur not in st:
+            ans = f2(cur)
+            break
+        else:
+            nums = f2(cur)
+            for i in range(n):
+                if nums[i] > 1:
+                    nums[i] -= 1
+                    heappush(h, (-get_sum(nums), f1(nums)))
+                    nums[i] += 1
+
+    print(*ans)
