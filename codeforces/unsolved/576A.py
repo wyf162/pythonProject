5995ea047c16c2b81f67bef7f925e5bea9a24593
yfwang <1626607072@qq.com>
2024-05-14 09:29:12 +0800

    576A game

---
+++ codeforces/unsolved/576A.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/5/14 9:01
+# @Author: yfwang
+# @File: 576A.py
+# https://codeforces.com/problemset/problem/576/A
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = I()
+for _tcn_ in range(tcn):
+    n, k = MI()
