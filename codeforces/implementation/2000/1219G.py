5f36ba3533ca9726eb7b75bbddac75b731d6986c
yfwang <1626607072@qq.com>
2024-05-17 09:58:59 +0800

    1219G implementation

--- codeforces/implementation/2000/1219G.py
+++ codeforces/implementation/2000/1219G.py
@@ -6,9 +6,9 @@
 # n^1.5
 
 import sys
-from heapq import heappop, heapify
+from heapq import nlargest
 
-sys.stdin = open('../../input.txt', 'r')
+# sys.stdin = open('../../input.txt', 'r')
 input = lambda: sys.stdin.readline().rstrip()
 I = lambda: int(input())
 MI = lambda: map(int, input().split())
@@ -22,48 +22,37 @@ mod2 = 998244353
 
 tcn = 1
 for _tcn_ in range(tcn):
-    n, m = MI()
-    mtx = [LI() for _ in range(n)]
-    if n <= 4 or m <= 4:
-        print(sum(sum(row) for row in mtx))
-        continue
-
-    row = [0] * n
-    col = [0] * m
-    for i in range(n):
-        for j in range(m):
-            row[i] += mtx[i][j]
-            col[j] += mtx[i][j]
+    m, n = LI()
+    grid = [LI() for _ in range(m)]
+    if m > n:
+        m, n = n, m
+        grid = list(zip(*grid))
+    rowsum = [sum(row) for row in grid]
+    colsum = [sum(col) for col in zip(*grid)]
 
     ans = 0
-    for i in range(n):
-        h = [-col[j] + mtx[i][j] for j in range(m)]
-        heapify(h)
-        ans = max(ans, row[i] - heappop(h) - heappop(h) - heappop(h))
+    ans = max(ans, sum(nlargest(4, rowsum)))
+    ans = max(ans, sum(nlargest(4, colsum)))
 
-    for j in range(m):
-        h = [-row[i] + mtx[i][j] for i in range(n)]
-        heapify(h)
-        ans = max(ans, col[j] - heappop(h) - heappop(h) - heappop(h))
+    for j, col in enumerate(zip(*grid)):
+        for i, x in enumerate(col):
+            rowsum[i] -= x
+        ans = max(ans, colsum[j] + sum(nlargest(3, rowsum)))
+        for i, x in enumerate(col):
+            rowsum[i] += x
+    for i, row in enumerate(grid):
+        for j, x in enumerate(row):
+            colsum[j] -= x
+        ans = max(ans, rowsum[i] + sum(nlargest(3, colsum)))
+        for j, x in enumerate(row):
+            colsum[j] += x
 
-    if n <= m:
-        for i1 in range(n):
-            for i2 in range(i1 + 1, n):
-                h = [-col[j] + mtx[i1][j] + mtx[i2][j] for j in range(m)]
-                heapify(h)
-                ans = max(ans, row[i1] + row[i2] - heappop(h) - heappop(h))
-    else:
-        for j1 in range(m):
-            for j2 in range(j1 + 1, m):
-                h = [-row[i] + mtx[i][j1] + mtx[i][j1] for i in range(n)]
-                heapify(h)
-                ans = max(ans, row[j1] + row[j2] - heappop(h) - heappop(h))
+    for i1 in range(m):
+        for i2 in range(i1 + 1, m):
+            for j in range(n):
+                colsum[j] -= grid[i1][j] + grid[i2][j]
+            ans = max(ans, rowsum[i1] + rowsum[i2] + sum(nlargest(2, colsum)))
+            for j in range(n):
+                colsum[j] += grid[i1][j] + grid[i2][j]
 
-    h = [-row[i] for i in range(n)]
-    heapify(h)
-    ans = max(ans, -heappop(h) - heappop(h) - heappop(h) - heappop(h))
-
-    h = [-col[j] for j in range(m)]
-    heapify(h)
-    ans = max(ans, -heappop(h) - heappop(h) - heappop(h) - heappop(h))
     print(ans)
