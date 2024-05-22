ce7a67b13f3e5df3dbd2130201972fc51145a78e
yfwang <1626607072@qq.com>
2024-05-22 13:42:52 +0800

    1716C unfinished

---
+++ codeforces/untag/1716C.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/5/22 13:17
+# @Author: yfwang
+# @File: 1716C.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = I()
+for _tcn_ in range(tcn):
+    m = I()
+    A1 = LI()
+    A2 = LI()
