b3aa884e5ce7f346ad7159edf176b4f559e9be58
yfwang <1626607072@qq.com>
2024-05-22 13:14:50 +0800

    update fenwickTree

--- doc/algo-template/fenwickTree.py
+++ doc/algo-template/fenwickTree.py
@@ -1,9 +1,6 @@
 # -*- coding : utf-8 -*-
 
 
-import typing
-
-
 class FenwickTree:
     """
     Reference: https://en.wikipedia.org/wiki/Fenwick_tree
@@ -14,7 +11,7 @@ class FenwickTree:
         self._n = n
         self.data = [0] * n
 
-    def add(self, p: int, x: typing.Any) -> None:
+    def add(self, p: int, x: int) -> None:
         assert 0 <= p < self._n
 
         p += 1
@@ -22,12 +19,12 @@ class FenwickTree:
             self.data[p - 1] += x
             p += p & -p
 
-    def sum(self, left: int, right: int) -> typing.Any:
+    def sum(self, left: int, right: int) -> int:
         assert 0 <= left <= right <= self._n
 
         return self._sum(right) - self._sum(left)
 
-    def _sum(self, r: int) -> typing.Any:
+    def _sum(self, r: int) -> int:
         s = 0
         while r > 0:
             s += self.data[r - 1]
