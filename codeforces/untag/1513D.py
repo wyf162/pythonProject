8d5c48dce5ebca3bd3a9f3b330b6f7eef14850df
wyf <1626607072@qq.com>
2024-05-30 07:53:20 +0800

    1513D

---
+++ codeforces/untag/1513D.py
@@ -0,0 +1,20 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/5/29 21:35
+# @Author: yefei.wang
+# @File: 1513D.py
+
+import math
+import sys
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+# sys.stdin = open('./../input.txt', 'r')
+
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+LI = lambda: list(map(int, input().split()))
+
+tcn = I()
+for _tcn_ in range(tcn):
+    n, p = MI()
+    a = LI()
+
