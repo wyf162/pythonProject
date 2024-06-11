6c33ac7f9d782cd05623e22cbb2d4d73922f50c0
yfwang <1626607072@qq.com>
2024-06-11 10:42:58 +0800

    725D heapq

---
+++ codeforces/data-structure/堆/725D.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/6/11 9:39
+# @Author: yfwang
+# @File: 725D.py
+
+import sys
+from heapq import heappop, heappush
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = 1
+for _tcn_ in range(tcn):
+    n = I()
+    teams = [LI() for _ in range(n)]
+    cur, weight = teams.pop(0)
+    teams.sort(key=lambda x: -x[0])
+
+    sl = 0
+    h = []
+    i = 0
+    ans = n
+    while i < n:
+        while i < n - 1 and teams[i][0] > cur:
+            heappush(h, (teams[i][1] - teams[i][0] + 1, teams[i][0]))
+            sl += 1
+            i += 1
+
+        ans = min(ans, sl + 1)
+        if h:
+            diff, x = heappop(h)
+            if diff <= cur:
+                cur = cur - diff
+                sl -= 1
+            else:
+                break
+        else:
+            ans = min(ans, 1)
+            break
+
+    print(ans)
