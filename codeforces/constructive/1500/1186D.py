85ebe866430e976bd9ceb91499b5cfa0ffab4bc7
wyf <1626607072@qq.com>
2024-06-15 17:39:17 +0800

    1186D constructive

---
+++ codeforces/constructive/1500/1186D.py
@@ -0,0 +1,57 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/6/15 14:30
+# @Author: yefei.wang
+# @File: 1186D.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = 1
+for _tcn_ in range(tcn):
+    n = I()
+    nums = []
+    x = 1
+    i1s = []
+    i2s = []
+    for i in range(n):
+        s = input()
+        s1, s2 = s.split('.')
+        if s2 == '00000':
+            nums.append(int(s1))
+        else:
+            if x == 1:
+                if s1.startswith('-'):
+                    nums.append(int(s1))
+                else:
+                    nums.append(int(s1) + 1)
+                i1s.append(i)
+            else:
+                if s1.startswith('-'):
+                    nums.append(int(s1) - 1)
+                else:
+                    nums.append(int(s1))
+                i2s.append(i)
+            x = -x
+    tot = sum(nums)
+    while tot > 0:
+        tot -= 1
+        i1 = i1s.pop()
+        nums[i1] -= 1
+
+    while tot < 0:
+        tot += 1
+        i2 = i2s.pop()
+        nums[i2] += 1
+
+    print('\n'.join(str(x) for x in nums))
