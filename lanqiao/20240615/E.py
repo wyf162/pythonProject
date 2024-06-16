fe4b25737fff9045781f091644eb2d7d975b938b
wyf <1626607072@qq.com>
2024-06-16 13:42:04 +0800

    update

--- lanqiao/20240615/E.py
+++ lanqiao/20240615/E.py
@@ -4,51 +4,10 @@
 # @File: E.py
 
 
+import math
 import os
 import sys
 from itertools import accumulate
-
-
-class Factorization:
-
-    def __init__(self, n):
-        self.N = n + 1
-        self.sieve = [-1] * self.N
-        for i in range(2, self.N):
-            if self.sieve[i] == -1:
-                for j in range(i, self.N, i):
-                    self.sieve[j] = i
-
-    def get_divisors(self, n):
-        divisors = [1]
-        while n != 1:
-            p = self.sieve[n]
-            cnt = 1
-            n //= p
-            while self.sieve[n] == p:
-                cnt += 1
-                n //= p
-            for i in range(len(divisors)):
-                for j in range(1, cnt + 1):
-                    divisors.append(divisors[i] * (p ** j))
-        return divisors
-
-    def get_factors(self, n):
-        factors = []
-        while n != 1:
-            p = self.sieve[n]
-            cnt = 1
-            n //= p
-            factors.append(p)
-            while self.sieve[n] == p:
-                cnt += 1
-                n //= p
-                factors.append(p)
-        return factors
-
-    def is_prime(self, n):
-        return self.sieve[n] == n
-
 
 # 请在此输入您的代码
 sys.stdin = open('../input.txt')
@@ -56,25 +15,40 @@ I = lambda: int(input())
 MI = lambda: map(int, input().split())
 LI = lambda: list(map(int, input().split()))
 
-n, k = MI()
+n, minlen = MI()
 A = LI()
 PA = list(accumulate(A, initial=0))
 
-mx = max(A)
-vis = [0] * (mx + 1)
-fact = Factorization(mx + 5)
-ans = PA[-1]
+# 同一右端点的子数组 有logM个不同的gcd
+# 要计算每个gcd 最远可以到哪里
 
-for g in range(mx, 0, -1):
-    i = j = 0
-    while j < n:
-        if A[j] % g == 0:
-            j += 1
-        else:
-            if j - i + 1 > k:
-                ans = max(ans, (PA[j] - PA[i]) * g)
-            j += 1
-            i = j
+tmp = []
+idx = []
+ans = 0
 
+for i in range(n):
+    k = len(tmp)
+    g = A[i]
 
+    for j in range(k - 1, -1, -1):
+        g = math.gcd(g, tmp[j])
+        tmp[j] = g
+
+    tmp.append(A[i])
+    idx.append(i)
+
+    ntmp = []
+    nidx = []
+
+    for j in range(k + 1):
+        if len(ntmp) == 0 or ntmp[-1] != tmp[j]:
+            ntmp.append(tmp[j])
+            nidx.append(idx[j])
+
+    tmp = ntmp
+    idx = nidx
+
+    for j in range(len(tmp)):
+        if i - idx[j] + 1 >= minlen:
+            ans = max(ans, (PA[i + 1] - PA[idx[j]]) * tmp[j])
 print(ans)
