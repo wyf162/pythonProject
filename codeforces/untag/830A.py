16b3790a1f2ce88d053bf6df006ca52d5bfb9825
wyf <1626607072@qq.com>
2024-06-18 22:07:31 +0800

    830A unfinished

---
+++ codeforces/untag/830A.py
@@ -0,0 +1,22 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/6/18 22:01
+# @Author: yefei.wang
+# @File: 830A.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = 1
+for _tcn_ in range(tcn):
+    n, k, p = MI()
