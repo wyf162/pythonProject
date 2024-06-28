2ac0ffa20d134d005cc79828119bc9e692234e04
wyf <1626607072@qq.com>
2024-06-28 21:02:36 +0800

    牛客小白月赛97

---
+++ nowcoder/contest/2024/0628/A.py
@@ -0,0 +1,25 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/6/28 19:06
+# @Author: yefei.wang
+# @File: A.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+n = I()
+nums = LI()
+cnt = [0] * 101
+for x in nums:
+    cnt[x] += 1
+
+YN(max(cnt) >= 3)
---
+++ nowcoder/contest/2024/0628/C.py
@@ -0,0 +1,65 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/6/28 19:11
+# @Author: yefei.wang
+# @File: C.py
+
+
+import math
+import sys
+
+
+class Factorial:
+    def __init__(self, N, mod) -> None:
+        N += 1
+        self.mod = mod
+        self.f = [1 for _ in range(N)]
+        self.g = [1 for _ in range(N)]
+        for i in range(1, N):
+            self.f[i] = self.f[i - 1] * i % self.mod
+        self.g[-1] = pow(self.f[-1], mod - 2, mod)
+        for i in range(N - 2, -1, -1):
+            self.g[i] = self.g[i + 1] * (i + 1) % self.mod
+
+    def fac(self, n):
+        return self.f[n]
+
+    def fac_inv(self, n):
+        return self.g[n]
+
+    def combi(self, n, m):
+        if n < m or m < 0 or n < 0: return 0
+        return self.f[n] * self.g[m] % self.mod * self.g[n - m] % self.mod
+
+    def permu(self, n, m):
+        if n < m or m < 0 or n < 0: return 0
+        return self.f[n] * self.g[n - m] % self.mod
+
+    def catalan(self, n):
+        return (self.combi(2 * n, n) - self.combi(2 * n, n - 1)) % self.mod
+
+    def inv(self, n):
+        return self.f[n - 1] * self.g[n] % self.mod
+
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+fact = Factorial(1005, mod)
+
+n, x = MI()
+m = math.isqrt(x)
+
+ans = 0
+while m >= n:
+    ans += fact.combi(m - 1, n - 1)
+    ans %= mod
+    m -= 1
+print(ans)
