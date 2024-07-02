b29b7a864defd3c5ec5ef435c21f65cb070b48fd
yfwang <1626607072@qq.com>
2024-07-02 15:07:50 +0800

    999D greedy

---
+++ codeforces/greedy/1900/999D.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/7/2 13:34
+# @Author: yfwang
+# @File: 999D.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = 1
+for _tcn_ in range(tcn):
+    n, m = MI()
+    A = LI()
+    c = n // m
+    cnt = [[] for _ in range(m)]
+    for i, a in enumerate(A):
+        r = a % m
+        cnt[r].append((a, i))
+
+    i1 = 0
+    i2 = m - 1
+    for i in range(m):
+        if len(cnt[i]) < c:
+            while i1 < i and len(cnt[i]) < c:
+                if len(cnt[i1]) > c:
+                    cnt[i].append(cnt[i1].pop())
+                else:
+                    i1 += 1
+
+            while i2 > i and len(cnt[i]) < c:
+                if len(cnt[i2]) > c:
+                    cnt[i].append(cnt[i2].pop())
+                else:
+                    i2 -= 1
+    # for i in range(m):
+    #     print(len(cnt[i]), end=' ')
+    # print()
+
+    B = [0] * n
+    for r in range(m):
+        for x, i in cnt[r]:
+            if x % m == r:
+                B[i] = x
+            else:
+                target = x // m * m + r
+                if target < x:
+                    target += m
+                B[i] = target
+    tot = sum(y - x for x, y in zip(A, B))
+    print(tot)
+    print(*B)
