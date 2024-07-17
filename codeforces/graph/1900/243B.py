072362bc3f5b2db051febc0a6a8e25f0b597b30e
yfwang <1626607072@qq.com>
2024-07-17 17:37:52 +0800

    243B graph

---
+++ codeforces/graph/1900/243B.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/7/17 14:33
+# @Author: yfwang
+# @File: 243B.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+
+
+def main():
+    n, m, h, t = MI()
+
+    path = [[] for _ in range(n)]
+
+    for i in range(m):
+        u, v = GMI()
+        path[u].append(v)
+        path[v].append(u)
+
+    used = [0] * n
+
+    def check(u, v):
+        if len(path[u]) <= h or len(path[v]) <= t:
+            return
+
+        used[u] = used[v] = -1
+        cnt = 0
+
+        for i in path[u]:
+            if not used[i]:
+                cnt += 1
+            used[i] |= 1
+
+        for i in path[v]:
+            if not used[i]:
+                cnt += 1
+            used[i] |= 2
+
+        if cnt >= h + t:
+            v1 = [i for i in path[u] if used[i] == 1]
+            v2 = [i for i in path[v] if used[i] == 2]
+            v3 = [i for i in path[u] if used[i] == 3]
+            chosen_1 = v1[:h]
+            chosen_2 = v2[:t]
+            for x in v3:
+                if len(chosen_1) < h:
+                    chosen_1.append(x)
+                elif len(chosen_2) < t:
+                    chosen_2.append(x)
+            print('YES')
+            print(u + 1, v + 1)
+            print(' '.join(str(i + 1) for i in chosen_1))
+            print(' '.join(str(i + 1) for i in chosen_2))
+            exit()
+
+        for i in path[u]:
+            used[i] = 0
+
+        for i in path[v]:
+            used[i] = 0
+
+    for u in range(n):
+        for v in path[u]:
+            check(u, v)
+
+    print('NO')
+
+
+main()
