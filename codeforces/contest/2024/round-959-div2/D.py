e773541b5bf687e4e8bf77c6fd87b911f6d61695
yfwang <1626607072@qq.com>
2024-07-19 16:21:58 +0800

    update 鸽笼

--- codeforces/contest/2024/round-959-div2/D.py
+++ codeforces/contest/2024/round-959-div2/D.py
@@ -2,52 +2,9 @@
 # @Time: 2024/7/18 23:23
 # @Author: yefei.wang
 # @File: D.py
+# Pigeonhole Principle
 
 import sys
-
-
-class Factorization:
-
-    def __init__(self, n):
-        self.N = n + 1
-        self.sieve = [-1] * self.N
-        for i in range(2, self.N):
-            if self.sieve[i] == -1:
-                for j in range(i, self.N, i):
-                    self.sieve[j] = i
-
-    def get_divisors(self, n):
-        divisors = [1]
-        while n != 1:
-            p = self.sieve[n]
-            cnt = 1
-            n //= p
-            while self.sieve[n] == p:
-                cnt += 1
-                n //= p
-            for i in range(len(divisors)):
-                for j in range(1, cnt + 1):
-                    divisors.append(divisors[i] * (p ** j))
-        return divisors
-
-    def get_factors(self, n, unique=True):
-        factors = []
-        while n != 1:
-            p = self.sieve[n]
-            cnt = 1
-            n //= p
-            factors.append(p)
-            while self.sieve[n] == p:
-                cnt += 1
-                n //= p
-                if unique:
-                    continue
-                factors.append(p)
-        return factors
-
-    def is_prime(self, n):
-        return self.sieve[n] == n
-
 
 input = lambda: sys.stdin.readline().rstrip()
 sys.stdin = open('../../../input.txt', 'r')
@@ -61,14 +18,26 @@ YN = lambda x: print('YES' if x else 'NO')
 mod = 1000000007
 mod2 = 998244353
 
-N = 2005
-fact = Factorization(N)
-
 tcn = I()
 for _tcn_ in range(tcn):
     n = I()
     A = LI()
-
-
-
-
+    print('YES')
+    used = [0] * n
+    res = []
+    for k in range(n - 1, 0, -1):
+        box = [-1] * k
+        for i in range(n):
+            if used[i]:
+                continue
+            m = A[i] % k
+            if box[m] == -1:
+                box[m] = i
+            else:
+                j = box[m]
+                res.append([i + 1, j + 1])
+                used[j] = 1
+                break
+    res.reverse()
+    for row in res:
+        print(*row)
