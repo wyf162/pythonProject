fb240c510854cf7c3d3e5824d36a46a924483008
wyf <1626607072@qq.com>
2024-07-20 22:33:40 +0800

    lc biweek-135

---
+++ leetcode/contest/2024/biweek-135/D.py
@@ -0,0 +1,11 @@
+# -*- coding : utf-8 -*-
+# @Time: 2024/7/20 22:31
+# @Author: yefei.wang
+# @File: D.py
+
+from typing import List
+
+
+class Solution:
+    def maximumScore(self, grid: List[List[int]]) -> int:
+        pass
