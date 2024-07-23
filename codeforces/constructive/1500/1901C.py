ecdcd0edb8a6f6db454d132e7583ba582e2d7460
yfwang <1626607072@qq.com>
2024-07-23 09:21:14 +0800

    1901C 构造

---
+++ codeforces/constructive/1500/1901C.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/7/23 8:59
+# @Author: yfwang
+# @File: 1901C.py
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.stdin = open('../../input.txt', 'r')
+I = lambda: int(input())
+MI = lambda: map(int, input().split())
+GMI = lambda: map(lambda x: int(x) - 1, input().split())
+LI = lambda: list(MI())
+TI = lambda: tuple(MI())
+LGMI = lambda: list(GMI())
+YN = lambda x: print('YES' if x else 'NO')
+mod = 1000000007
+mod2 = 998244353
+
+tcn = I()
+for _tcn_ in range(tcn):
+    n = I()
+    A = LI()
+    mi = min(A)
+    mx = max(A)
+    ans = (mx - mi).bit_length()
+    if 0 < ans <= n:
+        print(ans)
+        print(*[mi for _ in range(ans)])
+    else:
+        print(ans)
