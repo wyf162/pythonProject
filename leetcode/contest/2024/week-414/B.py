07de9aa09eebcb015f3f697a243d61c6b1023b1b
wyf <1626607072@qq.com>
2024-09-14 13:23:03 +0800

    lc 3281 二分

--- leetcode/contest/2024/week-414/B.py
+++ leetcode/contest/2024/week-414/B.py
@@ -9,13 +9,35 @@ from typing import List
 class Solution:
     def maxPossibleScore(self, start: List[int], d: int) -> int:
         n = len(start)
+        start.sort()
 
         def check(x):
-            low1, high1 = start[0], start[0] + d
-
+            coord = start[0]
             for i in range(1, n):
-                low2, high2 = start[i], start[i] + d
+                if start[i] >= coord + x:
+                    coord = start[i]
+                elif start[i] + d >= coord + x:
+                    coord = coord + x
+                else:
+                    return False
+            return True
+
+        L, R = 0, 2*10**9+5
+        while L<=R:
+            mid = (L+R)//2
+            if check(mid):
+                ans = mid
+                L = mid + 1
+            else:
+                R = mid - 1
+        return ans
 
 
 
 
+if __name__ == '__main__':
+    sol = Solution()
+    start = [6, 0, 3]
+    d = 2
+    ret = sol.maxPossibleScore(start, d)
+    print(ret)
