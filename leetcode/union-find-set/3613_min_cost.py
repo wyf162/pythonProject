abe7d1692962ed30e06131b0f896a821ef0266a9
yfwang <1626607072@qq.com>
2025-07-14 14:38:48 +0800

    lc 3613 dsu

---
+++ leetcode/union-find-set/3613_min_cost.py
@@ -0,0 +1,95 @@
+from typing import List
+import typing
+
+
+class DSU:
+    '''
+    Implement (union by size) + (path halving)
+
+    Reference:
+    Zvi Galil and Giuseppe F. Italiano,
+    Data structures and algorithms for disjoint set union problems
+    '''
+
+    def __init__(self, n: int = 0) -> None:
+        self._n = n
+        self.parent_or_size = [-1] * n
+        self.count_group = n
+
+    def merge(self, a: int, b: int) -> int:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        self.count_group -= 1
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def same(self, a: int, b: int) -> bool:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+
+        return a
+
+    def size(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self) -> typing.List[typing.List[int]]:
+        leader_buf = [self.leader(i) for i in range(self._n)]
+
+        result: typing.List[typing.List[int]] = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+
+        return list(filter(lambda r: r, result))
+
+
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]], k: int) -> int:
+        uf = DSU(n)
+
+        ans = 0
+        edges.sort(key=lambda x:x[2])
+        for u, v, w in edges:
+            if uf.count_group <= k:
+                break
+            else:
+                uf.merge(u, v)
+                ans = w
+        return ans
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    n = 5
+    edges = [[0, 1, 4], [1, 2, 3], [1, 3, 2], [3, 4, 6]]
+    k = 2
+    ret = sol.minCost(n, edges, k)
+    print(ret)
